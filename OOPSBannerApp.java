42c7ea07d249592ac792cb28197d1a2642c8f37a
HasherHH <ha2158@srmist.edu.in>
2026-03-10 00:48:21 +0530

    UC6 Added

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,16 +1,52 @@
-public class OOPSBannerApp{
-public static void main(String[] args){
-	for (String line : new String[] {
-        String.join("", "  *** ", "  *** ", " **** ", "  **** "),
-        String.join("", " * * ", " * * ", " * * ", " * "),
-        String.join("", " * * ", " * * ", " * * ", " * "),
-        String.join("", " * * ", " * * ", " **** ", "  *** "),
-        String.join("", " * * ", " * * ", " * ", "     * "),
-        String.join("", " * * ", " * * ", " * ", "     * "),
-        String.join("", "  *** ", "  *** ", " * ", " **** ")
-        }) {
-            System.out.println(line);
-        }
-}
+public class OOPSBannerApp {
 
+    // O pattern
+    static String[] getOPattern() {
+        return new String[]{
+                " *** ",
+                "*   *",
+                "*   *",
+                "*   *",
+                "*   *",
+                "*   *",
+                " *** "
+        };
+    }
+
+    // P pattern
+    static String[] getPPattern() {
+        return new String[]{
+                "**** ",
+                "*   *",
+                "*   *",
+                "**** ",
+                "*    ",
+                "*    ",
+                "*    "
+        };
+    }
+
+    // S pattern
+    static String[] getSPattern() {
+        return new String[]{
+                " ****",
+                "*    ",
+                "*    ",
+                " *** ",
+                "    *",
+                "    *",
+                "**** "
+        };
+    }
+
+    public static void main(String[] args) {
+
+        String[] O = getOPattern();
+        String[] P = getPPattern();
+        String[] S = getSPattern();
+
+        for (int i = 0; i < 7; i++) {
+            System.out.println(O[i] + " " + O[i] + " " + P[i] + " " + S[i]);
+        }
+    }
 }
